e447f04af7e7595e7b16c6c08750165bf7c1b6ff
takbirgazi <takbirgazibd@gmail.com>
2024-11-08 01:48:51 +0600

    Update: Some design

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
 import Hero from './src/components/Hero';
 
 const App = () => {
@@ -7,15 +7,15 @@ const App = () => {
     <View style={styles.mainBodyBackground}>
       <Hero />
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
 
 const styles = StyleSheet.create({
   mainBodyBackground: {
-    backgroundColor: "#090909",
-    height: "100%",
-    width: "100%",
-  }
-})
+    backgroundColor: '#090909',
+    height: '100%',
+    width: '100%',
+  },
+});
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,15 +11,15 @@ const Hero = () => {
                 <View style={styles.containerView}>
                     <Text style={styles.textColor}>Md. Takbir Gazi <Graduation name="graduation-cap" size={30} color="#4F8EF7" /></Text>
                     <Text style={styles.degText}>
-                        I a'm 
+                        I a'm Web Developer
                     </Text>
                 </View>
             </ImageBackground>
         </View>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
 
 const styles = StyleSheet.create({
     heroView: {
@@ -27,24 +27,24 @@ const styles = StyleSheet.create({
     },
     backGroundImage: {
         height: 250,
-        filter: "grayscale(100%)"
+        filter: 'grayscale(100%)',
     },
     containerView: {
         flex: 1,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#00000050",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#00000050',
         gap: 5,
 
     },
     textColor: {
-        color: "#ffff",
-        textAlign: "center",
+        color: '#ffff',
+        textAlign: 'center',
         fontSize: 24,
     },
     degText: {
-        color: "#ffff",
-        textAlign: "center",
-    }
-})
+        color: '#ffff',
+        textAlign: 'center',
+    },
+});
